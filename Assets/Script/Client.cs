a9087dbad37f6c320661614136f6c11888a3e31f
jaejinoh <wowls1231@naver.com>
2022-09-20 21:29:25 +0900

    2주차 파일 업로드

---
+++ Assets/Script/Client.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Client : MonoBehaviour
+{
+
+    Socket client;
+    string fromNetThread = "";
+    byte[] buffer = new byte[1024];
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //메인스레드와 네트워크 스레드가 분리되어 이렇게 작성해야함
+        if(fromNetThread.Length > 0)
+        {
+            GameObject.Find("UI Text").GetComponent<UnityEngine.UI.Text>().text += "\n" + fromNetThread;
+
+            fromNetThread = "";
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("클라이언트 접속중");
+
+
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+             
+            //ip주소로 서버에 접속하도록 설정
+            client.BeginConnect("127.0.0.1", 10000, null, client);
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            string msg = GameObject.Find("InputField").GetComponent<UnityEngine.UI.InputField>().text;
+
+            //msg 변수를 byte 단위로 변환
+            buffer = System.Text.ASCIIEncoding.ASCII.GetBytes(msg);
+
+            //버퍼를 보냄
+            client.BeginSend(buffer, 0, buffer.Length, SocketFlags.None, SendCallback, null);
+        }
+    }
+
+    void SendCallback(IAsyncResult result)
+    {
+        int len = client.EndSend(result);
+
+        print("보낸결과 : " + len);
+
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, null);
+    }
+
+    void RecvCallback(IAsyncResult result)
+    {
+        //Socket client = (Socket)result.AsyncState;
+        int len = client.EndReceive(result);
+
+        if (len > 0)
+        {
+            string recv = System.Text.ASCIIEncoding.ASCII.GetString(this.buffer);
+
+            //fromNetThread라는 변수에 recv값 저장
+            fromNetThread = recv;
+
+            print(recv);
+        }
+    }
+    }
---
+++ Assets/Script/Server.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Socket을 열기위함
+using System;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server : MonoBehaviour
+{
+
+    Socket server;
+    byte[] buffer = new byte[1024];
+
+    void Start()
+    {
+        //Socket 세팅을 위한 설정
+        server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        
+        //로컬서버 10000번 포트 연결
+        server.Bind(new IPEndPoint(IPAddress.Any, 10000));
+
+        //접속을 위해 버퍼 사이즈를 10으로 설정
+        server.Listen(10);
+
+
+        //Callback 함수 선언(비동기)
+        server.BeginAccept(AcceptCallback, server);
+        print("서버 실행");
+    } 
+
+
+
+    void AcceptCallback(IAsyncResult result)
+    {
+        //클라이언트와 연결된 Socket
+        Socket client = server.EndAccept(result);
+        IPEndPoint addr = ((IPEndPoint)client.RemoteEndPoint);
+
+        //Client의 정보도 가져옴
+        print(string.Format("{0}, {1}", addr.ToString(), addr.Port.ToString()));
+
+        //0부터 배열의 크기만큼 버퍼를 채움
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, client);
+
+        print("pending");
+    }
+
+    void RecvCallback(IAsyncResult result)
+    {
+        Socket client = (Socket)result.AsyncState;
+        //비동기적으로 데이터를 받는것을 멈춰야함
+
+        int len = client.EndReceive(result);
+
+
+        if (len > 0)
+        {
+            //len이 0보다 클때 버퍼에 있는걸 가져와 문자로 바꿔줌
+            string recv = System.Text.ASCIIEncoding.ASCII.GetString(this.buffer);
+
+            print(recv);
+
+           client.BeginSend(buffer, 0, len, 
+              SocketFlags.None, SendCallback, client);
+        }
+
+        //받은 버퍼를 서버에서 다시 돌려줌(에코서버 구성)
+        client.BeginReceive(buffer, 0, buffer.Length, SocketFlags.None, RecvCallback, client);
+    }
+
+
+    void SendCallback(IAsyncResult result)
+    {
+        Socket client = (Socket)result.AsyncState;
+        int len = client.EndSend(result);
+
+        print("서버에서 보낸결과 : " + len);
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
